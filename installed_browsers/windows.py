acb19e492b11d3a043f104cca7bc55c0cc53aaf2
undeflorate <swanslayer@gmail.com>
2025-11-22 00:59:07 +0100

    Fix failing windows unit test

--- installed_browsers/windows.py
+++ installed_browsers/windows.py
@@ -143,7 +143,7 @@ def get_details_of(name) -> Optional[Browser | str]:
         browser_name = POSSIBLE_BROWSER_NAMES[browser]
 
         if name == DUCKDUCKGO:
-            yield from _get_duckduckgo_details_from_registry()
+            yield _get_duckduckgo_details_from_registry()
             return
 
         yield _get_browser_details_from_registry(winreg.HKEY_CURRENT_USER, winreg.KEY_READ, browser_name)
@@ -164,7 +164,7 @@ def get_version_of(name) -> Optional[Version | str]:
         browser_name = POSSIBLE_BROWSER_NAMES[browser]
 
         if name == DUCKDUCKGO:
-            yield from _get_duckduckgo_version_from_registry()
+            yield _get_duckduckgo_version_from_registry()
             return
 
         yield _get_browser_version_from_registry(winreg.HKEY_CURRENT_USER, winreg.KEY_READ, browser_name)
@@ -481,6 +481,7 @@ def _get_duckduckgo_details_from_registry() -> Optional[Browser | str]:
                                     version=_create_browser_version(cmd),
                                     location=cmd
                                 )
+                                break
                 except OSError:  # pragma: no cover
                     description = subkey
     except FileNotFoundError:  # pragma: no cover
@@ -516,6 +517,7 @@ def _get_duckduckgo_version_from_registry() -> Optional[Version | str]:
                                 yield Version(
                                     version=_create_browser_version(cmd)
                                 )
+                                break
                 except OSError:  # pragma: no cover
                     description = subkey
     except FileNotFoundError:  # pragma: no cover
